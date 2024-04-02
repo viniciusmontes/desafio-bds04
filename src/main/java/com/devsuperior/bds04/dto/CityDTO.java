64e7594dbf7cb291513295da69adb47fbe99cd8f
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-04-02 14:19:31 -0300

    All tests passed

--- src/main/java/com/devsuperior/bds04/dto/CityDTO.java
+++ src/main/java/com/devsuperior/bds04/dto/CityDTO.java
@@ -2,14 +2,17 @@ package com.devsuperior.bds04.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotBlank;
+
 import com.devsuperior.bds04.entities.City;
 
 public class CityDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long id;
+	@NotBlank(message = "Campo requerido")
 	private String name;
-	
+
 	public CityDTO() {
 	}
 
@@ -17,7 +20,7 @@ public class CityDTO implements Serializable {
 		this.id = id;
 		this.name = name;
 	}
-	
+
 	public CityDTO(City entity) {
 		id = entity.getId();
 		name = entity.getName();
